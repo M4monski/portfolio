b5d0903c7dc33d17ab39b26638098d0acc13a1be
22-1918-942 Keith Harvey Angel <keithharvey.angel@cit.edu>
2025-05-16 15:55:00 +0800

    (Feat): Bio Portion Portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,84 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { IoDocumentTextOutline } from 'react-icons/io5';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-full h-full scroll-smooth ">
+      <header></header>
+      {/* BIO PART */}
+      <div className="bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
+        <main className="flex md:flex-row flex-col w-screen md:max-w-4/5 mx-auto py-4 h-screen items-center justify-center ">
+          {/* LEFT AREA */}
+          <div className="w-3/5 md:order-1 order-2 border-2 bg-white/20 rounded-4xl p-8">
+            <h1 className="font-bold text-[50px]">Keith Harvey P. Angel</h1>
+            <div className="flex gap-4 items-center">
+              <h2 className="text-[40px]">Frontend Developer </h2>
+              <div className="flex items-center gap-4 text-[30px]">
+                <a
+                  className="hover:scale-150 transition-all duration-500 hover:rotate-360 hover:rounded-full"
+                  target="_blank"
+                  href="https://www.linkedin.com/in/keith-harvey-angel-836a07365/"
+                >
+                  <FaLinkedin />
+                </a>
+                <a
+                  className="hover:scale-150 transition-transform duration-500 hover:rotate-360"
+                  target="_blank"
+                  href="https://github.com/M4monski"
+                >
+                  <FaGithub />
+                </a>
+                <a
+                  href="/resume/resume.pdf"
+                  download="resume.pdf"
+                  className="group flex items-center gap-1 border-white border-2 rounded-3xl p-2 transition-all hover:scale-110 hover:bg-white hover:text-purple-900 duration-500 hover:drop-shadow-2xl"
+                >
+                  <IoDocumentTextOutline className="group-hover:rotate-12 transition-all duration-500" />{' '}
+                  <span className="text-[18px]">Resume</span>
+                </a>
+              </div>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <p className="text-[20px] mt-2">
+              Frontend Developer eager to expand into full-stack roles, with
+              foundational experience in C# and .NET. I love transforming
+              designs into reality and continuously learning to overcome new
+              hurdles. Proficient in React.js, Next.js, JavaScript/TypeScript,
+              HTML, CSS/Tailwind, Node.js, and Firebase, I'm committed to
+              developing clean, impactful code. Excited to collaborate and build
+              innovative solutions!
+            </p>
+          </div>
+
+          {/* RIGHT AREA */}
+          <div className="w-2/5 md:order-2 order-1">
+            <div className="flex items-center justify-center">
+              <Image
+                className="rounded-full border-2 border-blue-50 drop-shadow-2xl"
+                src="/images/formalPicture.jpg"
+                width={300}
+                height={300}
+                alt="Character Picture"
+              ></Image>
+            </div>
+          </div>
+        </main>
+      </div>
+
+      {/* TECHNOLOGY */}
+      <div className="bg-white">
+        <main className="w-screen md:max-w-4/5 h-screen">
+          <h1 className="text-black">Technology Stack</h1>
+        </main>
+      </div>
+
+      {/* EXPERIENCE */}
+      <div className="bg-white">
+        <main className="w-screen md:max-w-4/5 h-screen"></main>
+      </div>
+
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
